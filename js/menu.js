68e8aed8f322a59fba6270f78391b7d517a5a0ca
Joan Kenedy <joankenedi9311@gmail.com>
2024-06-14 16:21:13 -0600

    ya-casi

--- js/menu.js
+++ js/menu.js
@@ -8,16 +8,18 @@ abrir.addEventListener("click", function () {
 const btnContacto = document.querySelector(".icono_principal");
 const caja = document.querySelector(".contactos");
 let contacto = document.querySelector(".contacto");
-btnContacto.addEventListener("click", function () {
-  caja.classList.toggle("aparecer");
-  contacto.style.display = "none";
-});
 
-// const modalInicio = document.querySelector(".modal_inicio");
-// const closeModal = document.querySelector(".close");
-// window.addEventListener("load", () => {
-//   modalInicio.classList.add("aparecer_modal");
-// });
-// closeModal.addEventListener("click", () => {
-//   modalInicio.classList.remove("aparecer_modal");
-// });
+if (screen.width < 767) {
+  btnContacto.addEventListener("click", function () {
+    caja.classList.toggle("aparecer");
+  });
+  console.log(screen.width);
+} else {
+  btnContacto.addEventListener("mouseover", function () {
+    caja.classList.add("aparecer");
+  });
+  btnContacto.addEventListener("click", function () {
+    caja.classList.remove("aparecer");
+  });
+  console.log(screen.width);
+}
