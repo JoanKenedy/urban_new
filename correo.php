14f7b403a77fb5726c088b1024dc3f0ef1a9de8f
Vladbraham <abrahamdelacruzhedo@gmail.com>
2024-07-26 13:19:04 -0500

    Actulizacion de contacto e index del formulario y foto de seguros

--- correo.php
+++ correo.php
@@ -1,19 +1,66 @@
 <?php
+  
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
 
-$destinatario = 'citasorthodentic@gmail.com';
-// este es al correo al que se enviara la información ontenida del formulario
+  require 'vendor/autoload.php';
+  $mail = new PHPMailer(true);
 
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$telefono = $_POST['tel'];
-$mensaje = $_POST['mensaje'];
+  $mail->IsHTML(true);
+  //$mail->IsSMTP(true);
 
-$header = "Enviado desde la página clinicadentalorthodentic.com ";
-$mensajeCompleto = $nombre . "\n" . $email . "\n" . $telefono . "\n" . $mensaje ;
+  // Gmail
 
-mail($destinatario, $header, $mensajeCompleto );
+  /* Variables para el uso de Mailtrap 
 
-echo "<script>alert('Correo enviado exitosamente')</script>";
-echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+  /* Variables necesarias para el envío de correos por GMAIL */
+//   $mail->SMTPAuth = true; // enable SMTP authentication
+//   $mail->SMTPSecure = "ssl"; // sets the prefix to the server
+//   $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+//   $mail->Port = 465; // set the SMTP port for the GMAIL server
+//   $mail->Username = "dentariosmx@gmail.com"; // GMAIL username
+//   $mail->Password = "piwdqjmtrvtpkfbb"; // GMAIL password
+
+$mail->isSMTP();
+$mail->Host = 'sandbox.smtp.mailtrap.io';
+$mail->SMTPAuth = true;
+$mail->Port = 2525;
+$mail->Username = '486c0a562c4fba';
+$mail->Password = '151b659b71a3e6';
+  // Verificamos la clave secreta 
+ 
+    
+    //Guardamos los datos escritos por el usuario
+    $nombre = $_POST['nombre'];
+    $emailUser = $_POST['email'];
+    $tel = $_POST['tel'];
+    $direccion = $_POST['direccion'];
+    $textarea = $_POST['textarea'];
+    //Estructura del correo
+    $mail->From = $correo;
+    $mail->FromName = $nombre;
+    $email_template = 'mail_template.html';
+    $email = 'linoulises.maldonadomich@gmail.com'; //linoulises.maldonadomich@gmail.com
+    $f_name = 'Dr. Lino';
+
+    $message = file_get_contents($email_template);
+    $message = str_replace('%nombre%', $nombre, $message);
+    $message = str_replace('%email%', $emailUser, $message);
+    $message = str_replace('%tel%', $tel, $message);
+    $message = str_replace('%direccion%', $direccion, $message);
+    $message = str_replace('%textarea%', $textarea, $message);
+
+
+    $mail->MsgHTML($message);
+    $mail->Subject = $subject;
+    $mail->AddAddress($email, $f_name);
+    $mail->send();
+    
+    // Send mail   
+    if (!$mail) {
+      echo 'Email not sent an error was encountered: ' . $mail->ErrorInfo;
+    } else {
+      header('Location:index.php');
+    }
+
 ?>
-
