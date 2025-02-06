a1762342115415d1e403304564d089cae8949152
luismorales780 <moralesalbarran780@gmail.com>
2025-02-06 16:56:18 -0600

    modificaciones 06-02-2025 luis morales

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,36 @@
-const abrir = document.querySelector(".abrirmenu");
-const menu = document.querySelector(".menu");
+(function() {
+  const abrir = document.querySelector(".abrirmenu");
+  const menu = document.querySelector(".menu");
 
-abrir.addEventListener("click", function () {
-  menu.classList.toggle("aparecer_menu");
-});
+  // Abrir y cerrar el menú al hacer clic
+  abrir.addEventListener("click", function () {
+    menu.classList.toggle("aparecer_menu");
+  });
 
-const btnContacto = document.querySelector(".icono_principal");
-const caja = document.querySelector(".contactos");
-let contacto = document.querySelector(".contacto");
+  // Cerrar el menú si se hace clic fuera de él
+  document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && !abrir.contains(event.target)) {
+      menu.classList.remove("aparecer_menu");
+    }
+  });
 
-if (screen.width < 767) {
-  btnContacto.addEventListener("click", function () {
-    caja.classList.toggle("aparecer");
-  });
-  console.log(screen.width);
-} else {
-  btnContacto.addEventListener("mouseover", function () {
-    caja.classList.add("aparecer");
-  });
-  btnContacto.addEventListener("click", function () {
-    caja.classList.remove("aparecer");
-  });
-  console.log(screen.width);
-}
+  const btnContacto = document.querySelector(".icono_principal");
+  const caja = document.querySelector(".contactos");
+  let contacto = document.querySelector(".contacto");
+
+  // Comportamiento para dispositivos móviles y de escritorio
+  if (screen.width < 767) {
+    btnContacto.addEventListener("click", function () {
+      caja.classList.toggle("aparecer");
+    });
+    console.log(screen.width);
+  } else {
+    btnContacto.addEventListener("mouseover", function () {
+      caja.classList.add("aparecer");
+    });
+    btnContacto.addEventListener("click", function () {
+      caja.classList.remove("aparecer");
+    });
+    console.log(screen.width);
+  }
+})();
